2a98a9d4a6103d6627d74303b0fdc38f7b1fe8d9
designali32 <alicengelcik@gmail.com>
2021-09-04 19:23:28 +0300

    homework_3

--- form.php
+++ form.php
@@ -24,3 +24,49 @@
  * 
  * > **Not: İsteyenler `app2.php` ve `form2.php` isminde dosyalar oluşturup sınıfa farklı özellikler kazandırabilir.
  */
+class Form{
+    //property tanımları yapıldı
+    protected string $action;
+    protected string $method;
+    protected $fields=[];
+
+    private function __construct(string $action,string $method){
+        //private constructor tanımlandı. Action ve method değerleri atandı.
+        $this->action=$action;
+        $this->method=$method;
+    }
+    //istenen static formlar tanımlandı
+    public static function createForm(string $action,string $method): Form{
+        //dışardan gelen metoda göre nesne oluşturuyor
+        return new static($action,$method);
+    }
+    public static function createGetForm(string $action,string $method="GET"): Form{
+    //get method nesnesi oluşturuyor
+        return new static($action,"GET");
+    }
+    public static function createPostForm(string $action,string $method="POST"): Form{
+    //post method nesnesi oluşturuyor
+        return new static($action,"POST");
+    }
+    public function addField(string $label,string $name,?string $defaultValue=null): void{
+        //field değerlerini aşağıda ekrana basarken kullanacağımız için kolaylık sağlaması adına isim verildi
+        //name label defaultValue değerleri diziye eklendi
+        $this->fields[$name]=[
+            "label"=>$label,
+            "name"=>$name,
+            "defaultValue"=>$defaultValue];
+    }
+    public function setMethod(string $method): void{
+        //method herhangi bir şekilde tekrar tetiklenmesi halinde methodu değiştiren setMethod methodu tanımlandı
+        $this->method=$method;
+    }
+    public function render(): void{
+        //metodlardan gelen değerlere göre html çıktısı ekrana basıldı
+        echo "<form method='$this->method' action='$this->action'>\n";
+        foreach ($this->fields as $field) {
+            //fields dizi olduğu için ekrana basılması için foreach döngüsü kullanılarak ekrana veriler basıldı
+            echo "<label for='$field[name]'>$field[label] </label>\n<input type='text' name='$field[name]' value='$field[defaultValue]' /> \n";
+        }
+            echo "<button type='submit'>Gönder</button>\n</form>\n";
+    }
+}
